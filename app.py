a2281ce765b1b11f250ba650d6cc3ea29fa5c543
Haldun Bayhantopcu <hb@haldun.me>
2011-11-13 23:35:07 +0200

    Stuff

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import tornado.web
 import tornado.wsgi
 from tornado.web import url
 
+from google.appengine.api import mail
+from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -18,25 +20,55 @@ class Application(tornado.wsgi.WSGIApplication):
   def __init__(self):
     handlers = [
       (r'/', IndexHandler),
-      # TODO Put your handlers here
+      (r'/home', HomeHandler),
+
+      # Inbound
+      (r'/_ah/mail/.+', InboundHandler),
     ]
     settings = dict(
+      debug=IS_DEV,
       static_path=os.path.join(os.path.dirname(__file__), "static"),
       template_path=os.path.join(os.path.dirname(__file__), 'templates'),
       xsrf_cookies=True,
-      # TODO Change this cookie secret
-      cookie_secret="asjidoh91239jasdasdasdasdasdkja8izxc21312sjdhsa/Vo=",
+      cookie_secret="m9i-asdj123ansdnzxjch7o23ij09iaodljansdiu123ojadsoo",
     )
     tornado.wsgi.WSGIApplication.__init__(self, handlers, **settings)
 
 
 class BaseHandler(tornado.web.RequestHandler):
-  pass
+  def get_current_user(self):
+    user = users.get_current_user()
+    if user:
+      user.admin = users.is_current_user_admin()
+    return user
+
+  def get_login_url(self):
+    return users.create_login_url(self.request.uri)
+
+  def render_string(self, template, **kwds):
+    return tornado.web.RequestHandler.render_string(
+        self, template, users=users, DEBUG=IS_DEV, **kwds)
 
 
 class IndexHandler(BaseHandler):
   def get(self):
     self.write('Hello from tornado on app engine')
+
+
+class HomeHandler(BaseHandler):
+  @tornado.web.authenticated
+  def get(self):
+    self.write("Naber lan? %s" % self.current_user)
+
+
+class InboundHandler(BaseHandler):
+  def initialize(self):
+    self.application.settings['xsrf_cookies'] = False
+
+  def post(self):
+    message = mail.InboundEmailMessage(self.request.body)
+    logging.info('%s' % message)
+
 
 def main():
   run_wsgi_app(Application())
--- models.py
+++ models.py
@@ -1,3 +1,22 @@
 from google.appengine.ext import db
 
-# TODO Put your models here
+class Account(db.Model):
+  user = db.UserProperty(auto_current_user_add=True, required=True)
+  email = db.EmailProperty(required=True) # key == <email>
+  created_at = db.DateTimeProperty(auto_now_add=True)
+  updated_at = db.DateTimeProperty(auto_now=True)
+
+
+class Address(db.Model):
+  account = db.ReferenceProperty(Account, collection_name='addresses')
+  token = db.StringProperty(required=True) # key
+  post_url = db.URLProperty(required=True)
+  created_at = db.DateTimeProperty(auto_now_add=True)
+  updated_at = db.DateTimeProperty(auto_now=True)
+
+
+class Message(db.Model):
+  account = db.ReferenceProperty(Account, collection_name='messages')
+  address = db.ReferenceProperty(Address, collection_name='messages')
+  raw_contents = db.TextProperty()
+  created_at = db.DateTimeProperty(auto_now_add=True)
